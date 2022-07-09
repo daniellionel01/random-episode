11dda74365ca6134c2bb5f5fdfa85885a8fbc420
Daniel Kurz <danielkurz03@protonmail.com>
2022-07-09 17:12:48 +0200

    use preact

---
+++ src/components/Friends.tsx
@@ -0,0 +1,21 @@
+import episodes from "../data/friends.json"
+
+export default function Friends() {
+  const index = Date.now() % episodes.length
+  const season = Math.floor(index)
+
+  const index2 = Date.now() % episodes[season].length
+  const episode = Math.floor(index2)
+
+  const ep = episodes[season][episode]
+
+  return (
+    <div>
+      <h1><u>Friends</u></h1>
+      <div id="episode">S{season+1} E{episode+1}</div>
+      <a href={`https://www.netflix.com/watch/${ep.id}`} target="_blank" id="link">
+        {ep.title}
+      </a>
+    </div>
+  )
+}
