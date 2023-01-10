0e0e1321d8a2c5c8cd33dc72819714d480042341
Daniel Kurz <danielkurz03@protonmail.com>
2023-01-10 18:42:40 +0100

    rm github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://daniellionel01.github.io",
-  base: "/random-episode",
   integrations: [preact()]
 });
